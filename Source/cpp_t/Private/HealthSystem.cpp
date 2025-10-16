efc249eae03b407a2ec61d7ae6b00ba9159d622d
tmdgus211 <tmdgus211@diverse-inc.co.kr>
2025-10-16 16:53:57 +0900

    c++테스트

---
+++ Source/cpp_t/Private/HealthSystem.cpp
@@ -0,0 +1,13 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "HealthSystem.h"
+
+HealthSystem::HealthSystem(float max = 100.0f)
+{
+
+}
+
+HealthSystem::~HealthSystem()
+{
+}
